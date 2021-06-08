2e45067a908ad5d158f712c47a4458158a87ce1a
Christian Bryant <74256156+Christian-Bryant2020@users.noreply.github.com>
2021-06-08 10:07:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of this file?',
+        name: 'title',
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(questions)
+}
 
 // Function call to initialize app
 init();
