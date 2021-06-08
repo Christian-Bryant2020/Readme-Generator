15e191562a558f0c7e56aba5bcd109b90d2e033b
Christian Bryant <74256156+Christian-Bryant2020@users.noreply.github.com>
2021-06-08 10:22:37 -0500

    Updated questions

--- index.js
+++ index.js
@@ -9,7 +9,53 @@ const questions = [
         type: 'input',
         message: 'What is the title of this file?',
         name: 'title',
-    }
+    },
+    {
+        type: 'input',
+        message: 'Enter a description:',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What are there installation instructions?',
+        name: 'install',
+    },
+    {
+        type: 'input',
+        message: 'What is the usage information?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'What are the contribution guidelines?',
+        name: 'contribution',
+    },
+    {
+        type: 'input',
+        message: 'What are the test instructions?',
+        name: 'test',
+    },
+    {
+        type: 'input',
+        message: 'Besides you, who contributed to this project?',
+        name: 'contributers',
+    },
+    {
+        type: 'input',
+        message: 'What is your gitHub username?',
+        name: 'gitHub',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+    },
+    {
+        type: 'list',
+        message: 'Choose a license for this project: ',
+        name: 'license',
+        choices: ['Apache', 'IBM', 'Mozilla', 'Eclipse'],
+      },
 ];
 
 // TODO: Create a function to write README file
